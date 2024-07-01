25e20dcfa72520f378e77968226b755f9bc07396
LinhNguyenAIO <linhnguyen.aio2024@gmail.com>
2024-07-01 14:52:32 +0700

    slpit function

--- levenshtein_distance.py
+++ levenshtein_distance.py
@@ -1,42 +1,32 @@
 import streamlit as st
 
 def levenshtein_distance(token1, token2):
-    distances = [[0]*(len(token2)+1) for i in range(len(token1)+1)]
+    dist = [[0] * (len(token2) + 1) for _ in range(len(token1) + 1)]
+    for i in range(len(token1) + 1):
+        dist[i][0] = i
+    for i in range(len(token2) + 1):
+        dist[0][i] = i
 
-    for t1 in range(len(token1) + 1):
-        distances[t1][0] = t1
-
-    for t2 in range(len(token2) + 1):
-        distances[0][t2] = t2
-
-    a = 0
-    b = 0
-    c = 0
-
-    for t1 in range(1, len(token1) + 1):
-        for t2 in range(1, len(token2) + 1):
-            if (token1[t1-1] == token2[t2-1]):
-                distances[t1][t2] = distances[t1 - 1][t2 - 1]
+    for i in range(1, len(token1) + 1):
+        for j in range(1, len(token2) + 1):
+            if (token1[i - 1] == token2[j - 1]):
+                dist[i][j] = dist[i - 1][j - 1]
             else:
-                a = distances[t1][t2 - 1]
-                b = distances[t1 - 1][t2]
-                c = distances[t1 - 1][t2 - 1]
+                dist[i][j] = min([dist[i][j - 1], dist[i - 1]
+                                 [j], dist[i - 1][j - 1]]) + 1
 
-                if (a <= b and a <= c):
-                    distances[t1][t2] = a + 1
-                elif (b <= a and b <= c):
-                    distances[t1][t2] = b + 1
-                else:
-                    distances[t1][t2] = c + 1
+    return dist[len(token1)][len(token2)]
 
-    return distances[len(token1)][len(token2)]
 
 def load_vocab(file_path):
     with open(file_path, 'r') as f:
         lines = f.readlines()
     words = sorted(set([line.strip().lower() for line in lines]))
     return words
+
+
 vocabs = load_vocab(file_path='./data/vocab.txt')
+
 
 def main():
     st.title("Word Correction using Levenshtein Distance")
@@ -48,18 +38,20 @@ def main():
         leven_distances = dict()
         for vocab in vocabs:
             leven_distances[vocab] = levenshtein_distance(word, vocab)
-        
+
         # sorted by distance
-        sorted_distences = dict(sorted(leven_distances.items(), key=lambda item: item[1]))
+        sorted_distences = dict(
+            sorted(leven_distances.items(), key=lambda item: item[1]))
         correct_word = list(sorted_distences.keys())[0]
         st.write('Correct word: ', correct_word)
 
         col1, col2 = st.columns(2)
         col1.write('Vocabulary:')
         col1.write(vocabs)
-        
+
         col2.write('Distances:')
         col2.write(sorted_distences)
+
 
 if __name__ == "__main__":
     # main()
